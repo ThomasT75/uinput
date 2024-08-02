53cf9067b3f18167b7f078a167a4a87a6b9c42b6
thomast75 <62158572+ThomasT75@users.noreply.github.com>
2024-08-02 02:05:56 -0300

    Test for memory layout of FFEffect struct

--- uinput_test.go
+++ uinput_test.go
@@ -4,7 +4,28 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"unsafe"
 )
+
+// use go test -v -run TestFFEffectMemoryLayout to see what went wrong
+// and yes this was a problem so making a test for it
+func TestFFEffectMemoryLayout(t *testing.T) {
+  var i FFEffect
+  t.Log("ff_effect\t size\t offset") 
+  t.Logf("Type\t\t %v\t %v", unsafe.Sizeof(i.Type), unsafe.Offsetof(i.Type))
+  t.Logf("ID\t\t %v\t %v", unsafe.Sizeof(i.ID), unsafe.Offsetof(i.ID))
+  t.Logf("Direction\t %v\t %v", unsafe.Sizeof(i.Direction), unsafe.Offsetof(i.Direction))
+  t.Logf("Trigger\t\t %v\t %v", unsafe.Sizeof(i.Trigger), unsafe.Offsetof(i.Trigger))
+  t.Logf("Replay\t\t %v\t %v", unsafe.Sizeof(i.Replay), unsafe.Offsetof(i.Replay))
+  t.Logf("u\t\t %v\t %v", unsafe.Sizeof(i.u), unsafe.Offsetof(i.u))
+  t.Logf("Whole Size\t %v", unsafe.Sizeof(i))
+  if unsafe.Offsetof(i.u) != 16 {
+    t.Fatalf("Expected Union Offset in FFEffect to be 16\nActual: %v", unsafe.Offsetof(i.u))
+  }
+  if unsafe.Sizeof(i) != 48 {
+    t.Fatalf("Expected Size of FFEffect to be 48\nActual: %v", unsafe.Sizeof(i))
+  }
+}
 
 func TestValidateDevicePathEmptyPathPanics(t *testing.T) {
 	expected := "device path must not be empty"
